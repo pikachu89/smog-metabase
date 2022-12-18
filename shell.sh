4d9395b09686206a913bf5644635e5b7296ee8c3
Abdul Saboor <abdulsaboor938@gmail.com>
2022-12-18 01:17:32 +0000

    OK

--- shell.sh
+++ shell.sh
@@ -1,3 +1,6 @@
 cd ./metabase
-wget https://downloads.metabase.com/v0.44.6/metabase.jar
+if [ ! -f metabase.jar ]; then
+    echo "metabase.jar not found. Downloading..."
+    wget https://downloads.metabase.com/v0.44.6/metabase.jar
+fi
 java -jar metabase.jar
